8448337ddcbd004b5f49f8ec0a76e1fcfd96851e
JoMingyu <city7310@naver.com>
2018-02-06 14:43:30 +0900

    [UPDATE] Remove samples, add JWT related configs

--- Server/config/__init__.py
+++ Server/config/__init__.py
@@ -1,14 +1,19 @@
+from datetime import timedelta
 import os
 
 
 class Config(object):
-    DOMAIN = None
-    PORT = 3000
+    DOMAIN = 'dsm2015.cafe24.com'
+    PORT = 80
 
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
     # Secret key for any 3-rd party libraries
 
-    SERVICE_NAME = 'Flask Large Application Example'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=365)
+    JWT_HEADER_TYPE = 'JWT'
+
+    SERVICE_NAME = 'dms'
 
     SWAGGER = {
         'title': SERVICE_NAME,
