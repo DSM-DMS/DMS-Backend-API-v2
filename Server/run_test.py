f1bb3e7947610345718c86f9e922bcfc16a39b44
JoMingyu <city7310@naver.com>
2018-02-07 08:49:45 +0900

    [UPDATE] Add '--testing' argument

--- Server/run_test.py
+++ Server/run_test.py
@@ -1,5 +1,15 @@
+from argparse import ArgumentParser
+
 import unittest as ut
 
+from server import app
+
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--testing')
+    args = parser.parse_args()
+
+    app.config['TESTING'] = True if args.skip_apply_time_limits else False
+
     all_tests = ut.TestLoader().discover('tests', '*.py')
     ut.TextTestRunner().run(all_tests)
