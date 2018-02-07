5a99e0cb32e4d6d6a75931c1fb06f6a20d8b5583
JoMingyu <city7310@naver.com>
2018-02-07 17:16:18 +0900

    [UPDATE] Add 'After Test' to existing TCs

--- Server/tests/views/account_admin/account_control.py
+++ Server/tests/views/account_admin/account_control.py
@@ -1,5 +1,7 @@
 import json
 
+from app.models.account import SignupWaitingModel
+
 from tests.views import TCBase
 
 
@@ -8,24 +10,27 @@ class TestAccountControl(TCBase):
         """
         TC about existing student account deletion
 
-        - Preparations
+        - Before Test
         None
 
-        - Process
+        - Test
         Take UUID with signed student account deletion
         * Test passes : status code 201 with new UUID(length 4) in response
 
-        - Exception Tests
+        - Exception Test
         Non-existing student number
         * Test passes : status code 204
 
         Forbidden with student access token
         * Test passes : status code 403
-        """
-        # -- Preparations --
-        # -- Preparations --
 
-        # -- Process --
+        - After Test
+        Clear SignupWaitingModel
+        """
+        # -- Before Test --
+        # -- Before Test --
+
+        # -- Test --
         res = self.client.delete(
             '/admin/account-control/student',
             data=json.dumps({'number': 1111}),
@@ -37,9 +42,9 @@ class TestAccountControl(TCBase):
         response_data = json.loads(res.data.decode())
         self.assertTrue('uuid' in response_data)
         self.assertTrue(len(response_data['uuid']) == 4)
-        # -- Process --
+        # -- Test --
 
-        # -- Exception Tests --
+        # -- Exception Test --
         res = self.client.delete(
             '/admin/account-control/student',
             data=json.dumps({'number': 0000}),
@@ -54,24 +59,31 @@ class TestAccountControl(TCBase):
             headers={'Authorization': self.student_access_token}
         )
         self.assertTrue(res.status_code == 403)
-        # -- Exception Tests --
+        # -- Exception Test --
+
+        # -- After Test --
+        SignupWaitingModel.objects.delete()
+        # -- After Test --
 
     def testB_deleteAlreadySignupWaitingStudent(self):
         """
         TC about already signup waiting student account deletion
 
-        - Preparations
+        - Before Test
         Remove student account, move student data to signup_waiting
         * DELETE /admin/account-control/student
 
-        - Process
+        - Test
         Get just UUID with already signup waiting student number
         * Test passes : status code 200 with UUID(length 4) in response
 
-        - Exception Tests
+        - Exception Test
         None
+
+        - After Test
+        Clear SignupWaitingModel
         """
-        # -- Preparations --
+        # -- Before Test --
         res = self.client.delete(
             '/admin/account-control',
             data=json.dumps({'number': 1111}),
@@ -79,9 +91,9 @@ class TestAccountControl(TCBase):
             headers={'Authorization': self.admin_access_token},
         )
         self.assertTrue(res.status_code == 201)
-        # -- Preparations --
+        # -- Before Test --
 
-        # -- Process --
+        # -- Test --
         res = self.client.delete(
             '/admin/account-control',
             data=json.dumps({'number': 1111}),
@@ -93,7 +105,11 @@ class TestAccountControl(TCBase):
         response_data = json.loads(res.data.decode())
         self.assertTrue('uuid' in response_data)
         self.assertTrue(len(response_data['uuid']) == 4)
-        # -- Process --
+        # -- Test --
 
-        # -- Exception Tests --
-        # -- Exception Tests --
+        # -- Exception Test --
+        # -- Exception Test --
+
+        # -- After Test --
+        SignupWaitingModel.objects.delete()
+        # -- After Test --
